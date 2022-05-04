c766ca750d9ad838086643046fbbefe7ee9be197
Daniel Samko <gekk0n.ua@gmail.com>
2022-05-05 02:57:00 +0300

    feat: finish minimal scope

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
+import {createHeader} from './layout.js'
+import {enKeyCode, enKey} from './keynames.js'
+createHeader()
+let SERVICE_KEYS = document.querySelectorAll('.Backspace, .Tab, .CapsLock, .Enter, .ShiftLeft, .ShiftRight, .ShiftRight, .ControlLeft, .MetaLeft, .AltLeft, .AltRight, .MetaRight, .ControlRight') 
 
 const INPUT = document.createElement('textarea')
 INPUT.classList.add('input')
 document.body.append(INPUT)
-// setTimeout(function() {
-//     INPUT.focus();
-// }, 0);
-
-
+INPUT.addEventListener('keydown', event => event.preventDefault())
 
 const KBRD = document.createElement('div')
 KBRD.classList.add('keyboard')
 document.body.append(KBRD)
 
-for(let i = 0; i < 61; i++){
+
+for(let i = 0; i < enKeyCode.length; i++){
     const KEY = document.createElement ('div')
     KEY.classList.add('key')
+    KEY.classList.add(`${enKeyCode[i]}`)
     KBRD.append(KEY)
-    KEY.dataset.code = 'Digit1'
+    KEY.dataset.code = `${enKeyCode[i]}`
+    KEY.innerText = `${enKey[i]}`
+}
+const KEYS = document.querySelectorAll('.key')
+
+
+
+
+document.addEventListener('keydown', keyInput )
+SERVICE_KEYS.forEach((el) => el.removeEventListener('keydown', keyInput))
+document.addEventListener('keydown', (event) => {
+    document.querySelector(`[data-code = '${event.code}']`).classList.add('pressed')
+})
+document.addEventListener('keyup', (event) => {
+    document.querySelector(`[data-code = '${event.code}']`).classList.remove('pressed')
+})
+
+KEYS.forEach( el => el.addEventListener('click', clickInput))
+
+
+const WIN = document.querySelectorAll('.MetaLeft, .MetaRight')
+WIN.forEach(el => el.innerText = 'Win')
+const SPACE = document.querySelector('.Space')
+SPACE.innerText = ''
+const CTRL = document.querySelectorAll('.ControlLeft, .ControlRight')
+CTRL.forEach(el => el.innerText = 'Ctrl')
+
+
+
+SERVICE_KEYS = document.querySelectorAll('.Backspace, .Tab, .CapsLock, .Enter, .ShiftLeft, .ShiftRight, .ShiftRight, .ControlLeft, .MetaLeft, .AltLeft, .AltRight, .MetaRight, .ControlRight') 
+let serviceCodes = []
+for (let i = 0; i < SERVICE_KEYS.length; i++){
+    serviceCodes.push(SERVICE_KEYS[i].dataset.code);
 }
 
-
-const KEY = document.querySelector('.key')
-KEY.innerText = '1'
-
-INPUT.addEventListener('keydown', (event) => {
-        let code = event.code
-        if (KEY.dataset.code = `${code}`) {
-        KEY.style.backgroundColor = 'red'
-        }
-    
-    // INPUT.value += event.key
-    console.log(event.code)
-})
-
-
-KEY.addEventListener('click', (event) => {
+function keyInput (event) {
+    if (event.code !== serviceCodes.find(el => el === event.code))
+    INPUT.value += event.key
+}
+function clickInput (event) {
+    if (event.target.dataset.code !== serviceCodes.find(el => el === event.target.dataset.code))
     INPUT.value += event.target.innerText
-})
+}
